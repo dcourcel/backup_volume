2f886c6819f4b46fd2724e91a3ee157cb54490c6
Dominik Courcelles <dominik.courcelles@gmail.com>
2020-11-13 20:08:26 -0500

    Add compression options in script. Put the compression in a condition bloc to terminate the execution even if the compression fails.

--- backup.sh
+++ backup.sh
@@ -9,8 +9,26 @@ if [ -z "$DESTINATION" ]; then
     echo '$DESTINATION' is not defined
     exit 1
 fi;
+
+if [ -z "$COMPRESSION" -o "$COMPRESSION" = "bz2" ]; then
+    COMPRESS_PARAM=-j
+    COMPRESS_EXT=.bz2
+elif [ "$COMPRESSION" = "gz" ]; then
+    COMPRESS_PARAM=-z
+    COMPRESS_EXT=.gz
+elif [ "$COMPRESSION" = "tar" ]; then
+    COMPRESS_PARAM=
+    COMPRESS_EXT=
+else
+    echo "Invalid compression parameter '$COMPRESSION'."
+    exit 1
+fi;
+
 echo "----------------------------------------"
 echo "Begin $BACKUP_NAME backup."
-mkdir -p "$DESTINATION" && cd "$SOURCE" && tar -c -f "${DESTINATION}/${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M-%S).tar.bz2" -j .
-echo "$BACKUP_NAME backup completed"
+if mkdir -p "$DESTINATION" && cd "$SOURCE" && tar -c -f "${DESTINATION}/${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M-%S).tar${COMPRESS_EXT}" $COMPRESS_PARAM . ; then
+    echo "$BACKUP_NAME backup completed"
+else
+    echo "$BACKUP_NAME backup failed"
+fi;
 echo -e "----------------------------------------\n" 
